a2910a52033be48cdee1c6ed794de52ef6a16f3f
yNiklas <niklas@magic-computer.de>
2026-06-02 22:49:53 +0200

    By-hand integration for tendons

--- bioptim/examples/toy_examples/tendon_driven_ocp/contact_tendon_finger.py
+++ bioptim/examples/toy_examples/tendon_driven_ocp/contact_tendon_finger.py
@@ -2,58 +2,82 @@ import faulthandler
 
 import numpy as np
 from biorbd import contact_index
+from casadi import MX
 
 faulthandler.enable()
 
 from bioptim import TendonBiorbdModel, OptimalControlProgram, Objective, ObjectiveFcn, BoundsList, InitialGuessList, \
     Solver, ConstraintList, ConstraintFcn, Node, ContactType, ObjectiveList, PhaseTransitionList, PhaseTransitionFcn, \
-    CostType, TorqueBiorbdModel
+    CostType, TorqueBiorbdModel, PenaltyController, BiMappingList, DynamicsOptions
 from bioptim.examples.utils import ExampleUtils
 
 def prepare_single_phase_ocp(biorbd_model_path: str,) -> OptimalControlProgram:
     bio_model = TendonBiorbdModel(biorbd_model_path, contact_types=[ContactType.RIGID_EXPLICIT])
 
     objective_functions = ObjectiveList()
-    #objective_functions.add(Objective(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tendons", phase=0))
-    objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_TIME)
+    objective_functions.add(Objective(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tendons", phase=0))
+    #objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_TIME)
 
     constraints = ConstraintList()
     constraints.add(
         ConstraintFcn.TRACK_EXPLICIT_RIGID_CONTACT_FORCES,
         min_bound=0,
         max_bound=np.inf,
-        node=Node.END
+        node=Node.ALL,
+        contact_index=0
     )
-    constraints.add(
-        ConstraintFcn.SUPERIMPOSE_MARKERS,
+    #constraints.add(
+    #    ConstraintFcn.SUPERIMPOSE_MARKERS,
+    #    node=Node.END,
+    #    first_marker="endeffector",
+    #    second_marker="endeffector_final"
+    #)
+    constraints.add(  # Reach ground with distal at the end of first phase
+        ConstraintFcn.TRACK_MARKERS,
         node=Node.END,
-        first_marker="endeffector",
-        second_marker="endeffector_final"
+        marker_index=bio_model.marker_index("endeffector"),
+        index=2,
+        min_bound=0,
+        #max_bound=0.001
     )
+
+    dof_mapping = BiMappingList()
+    #dof_mapping.add("q", to_second=[None, None, None, 0, 1], to_first=[3, 4])
+    #dof_mapping.add("qdot", to_second=[None, None, None, 0, 1], to_first=[3, 4])
+    #dof_mapping.add("tendons", to_second=[None, None, None, 0, 1], to_first=[3,4])
 
     x_bounds = BoundsList()
     x_bounds.add("q", bio_model.bounds_from_ranges("q"))
     x_bounds.add("qdot", bio_model.bounds_from_ranges("qdot"))
-    x_bounds[0]["q"][:, 0] = 0
+    x_bounds["q"][:, 0] = 0
     #x_bounds[0]["q"][1, 0] = 0.5
     #x_bounds[1]["q"][0, -1] = 1.2
-    #x_bounds[1]["q"][1, -1] = 0.63
-    x_bounds[0]["qdot"][:, 0] = 0
+    #x_bounds[1]["q"][1, -1] = 0.6
+    #x_bounds["qdot"][:, 0] = 0
     #x_bounds["qdot"][:, -1] = 0
 
     u_bounds = BoundsList()
-    u_bounds.add("tendons", min_bound=[0], max_bound=[400])
+    u_bounds.add("tendons", min_bound=[0], max_bound=[40])
+
+    u_init = InitialGuessList()
+    u_init.add("tendons", [0.5])
 
     return OptimalControlProgram(
         bio_model,
         n_shooting=50,
-        phase_time=1,
+        phase_time=0.5,
         objective_functions=objective_functions,
         constraints=constraints,
+        #variable_mappings=dof_mapping,
         x_bounds=x_bounds,
         u_bounds=u_bounds,
+        u_init=u_init
     )
 
+def track_z_coordinate_of_marker(controller: PenaltyController, marker_name: str) -> MX:
+    marker_idx = controller.model.marker_index(marker_name)
+    markers_pos = controller.model.markers()(controller.states["q"].cx, controller.parameters.cx)
+    return markers_pos[2, marker_idx]
 
 
 def prepare_ocp(biorbd_model_path: str,) -> OptimalControlProgram:
@@ -62,14 +86,42 @@ def prepare_ocp(biorbd_model_path: str,) -> OptimalControlProgram:
         TendonBiorbdModel(biorbd_model_path, contact_types=[ContactType.RIGID_EXPLICIT]),
     )
 
+    endeffector_marker_idx = bio_model[0].marker_index("endeffector")
+
     objective_functions = ObjectiveList()
     #objective_functions.add(Objective(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tendons", phase=0))
-    #objective_functions.add(Objective(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tendons", phase=0))
+    #objective_functions.add(Objective(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tendons", phase=1))
+    objective_functions.add(Objective(ObjectiveFcn.Mayer.TRACK_MARKERS, marker_index=endeffector_marker_idx, phase=0))
     objective_functions.add(Objective(ObjectiveFcn.Mayer.MINIMIZE_TIME, phase=0))
     objective_functions.add(Objective(ObjectiveFcn.Mayer.MINIMIZE_TIME, phase=1))
 
     constraints = ConstraintList()
-    constraints.add(
+    #constraints.add(
+    #    track_z_coordinate_of_marker,
+    #    node=Node.ALL_SHOOTING,
+    #    marker_name="base_contact_right_marker",
+    #    min_bound=0,
+    #    max_bound=0.01
+    #)
+    #constraints.add(
+    #    ConstraintFcn.TRACK_MARKERS,
+    #    node=Node.ALL_SHOOTING,
+    #    marker_index=bio_model[0].marker_index("base_contact_left_marker"),
+    #    index=2,
+    #    min_bound=0,
+    #    max_bound=0.01,
+    #    phase=0
+    #)
+    constraints.add( # Keep base heel on the ground
+        ConstraintFcn.TRACK_MARKERS,
+        node=Node.ALL_SHOOTING,
+        marker_index=bio_model[0].marker_index("base_contact_right_marker"),
+        index=2,
+        min_bound=0,
+        max_bound=0.01,
+        phase=0
+    )
+    constraints.add( # Unilateral base toe contact (first phase)
         ConstraintFcn.TRACK_EXPLICIT_RIGID_CONTACT_FORCES,
         min_bound=0,
         max_bound=np.inf,
@@ -77,7 +129,7 @@ def prepare_ocp(biorbd_model_path: str,) -> OptimalControlProgram:
         phase=0,
         contact_index=1
     )
-    constraints.add(
+    constraints.add( # Unilateral base heel contact (first phase)
         ConstraintFcn.TRACK_EXPLICIT_RIGID_CONTACT_FORCES,
         min_bound=0,
         max_bound=np.inf,
@@ -85,15 +137,15 @@ def prepare_ocp(biorbd_model_path: str,) -> OptimalControlProgram:
         phase=0,
         contact_index=2
     )
-    constraints.add(
-        ConstraintFcn.TRACK_EXPLICIT_RIGID_CONTACT_FORCES,
-        min_bound=0,
-        max_bound=np.inf,
-        node=Node.ALL_SHOOTING,
-        phase=1,
-        contact_index=1
-    )
-    constraints.add(
+    #constraints.add(
+    #    ConstraintFcn.TRACK_EXPLICIT_RIGID_CONTACT_FORCES,
+    #    min_bound=0,
+    #    max_bound=np.inf,
+    #    node=Node.ALL_SHOOTING,
+    #    phase=1,
+    #    contact_index=1
+    #)
+    constraints.add( # Unilateral base heel contact (second phase)
         ConstraintFcn.TRACK_EXPLICIT_RIGID_CONTACT_FORCES,
         min_bound=0,
         max_bound=np.inf,
@@ -101,7 +153,7 @@ def prepare_ocp(biorbd_model_path: str,) -> OptimalControlProgram:
         phase=1,
         contact_index=2
     )
-    constraints.add(
+    constraints.add( # Unilateral distal contact (second phase)
         ConstraintFcn.TRACK_EXPLICIT_RIGID_CONTACT_FORCES,
         min_bound=0,
         max_bound=np.inf,
@@ -109,29 +161,41 @@ def prepare_ocp(biorbd_model_path: str,) -> OptimalControlProgram:
         phase=1,
         contact_index=3
     )
-    constraints.add(
-        ConstraintFcn.SUPERIMPOSE_MARKERS,
+    #constraints.add(
+    #    ConstraintFcn.SUPERIMPOSE_MARKERS,
+    #    node=Node.END,
+    #    first_marker="endeffector",
+    #    second_marker="endeffector_contact_inlet",
+    #    phase=0
+    #)
+    constraints.add( # Reach ground with distal at the end of first phase
+        ConstraintFcn.TRACK_MARKERS,
         node=Node.END,
-        first_marker="endeffector",
-        second_marker="endeffector_contact_inlet",
+        marker_index=endeffector_marker_idx,
+        index=2,
+        min_bound=0,
         phase=0
     )
-    constraints.add(
-        ConstraintFcn.SUPERIMPOSE_MARKERS,
-        node=Node.END,
-        first_marker="endeffector",
-        second_marker="endeffector_final",
-        phase=1
-    )
+    #constraints.add( # Reach final location at the end of second phase
+    #    ConstraintFcn.SUPERIMPOSE_MARKERS,
+    #    node=Node.END,
+    #    first_marker="endeffector",
+    #    second_marker="endeffector_final",
+    #    phase=1
+    #)
+
+    dof_mapping = BiMappingList()
+    dof_mapping.add("q", to_second=[None, None, None, 0, 1], to_first=[3,4])
+    dof_mapping.add("qdot", to_second=[None, None, None, 0, 1], to_first=[3,4])
 
     phase_transitions = PhaseTransitionList()
     phase_transitions.add(PhaseTransitionFcn.IMPACT, phase_pre_idx=0)
 
     x_bounds = BoundsList()
-    x_bounds.add("q", bio_model[0].bounds_from_ranges("q"), phase=0)
-    x_bounds.add("q", bio_model[0].bounds_from_ranges("q"), phase=1)
-    x_bounds.add("qdot", bio_model[0].bounds_from_ranges("qdot"), phase=0)
-    x_bounds.add("qdot", bio_model[0].bounds_from_ranges("qdot"), phase=1)
+    x_bounds.add("q", bio_model[0].bounds_from_ranges("q", mapping=dof_mapping), phase=0)
+    x_bounds.add("q", bio_model[0].bounds_from_ranges("q", mapping=dof_mapping), phase=1)
+    x_bounds.add("qdot", bio_model[0].bounds_from_ranges("qdot", mapping=dof_mapping), phase=0)
+    x_bounds.add("qdot", bio_model[0].bounds_from_ranges("qdot", mapping=dof_mapping), phase=1)
     x_bounds[0]["q"][:, 0] = 0
     #x_bounds[0]["q"][0, 0] = 0.7
     #x_bounds[0]["q"][1, 0] = 0.5
@@ -141,22 +205,24 @@ def prepare_ocp(biorbd_model_path: str,) -> OptimalControlProgram:
     #x_bounds["qdot"][:, -1] = 0
 
     u_bounds = BoundsList()
-    u_bounds.add("tendons", min_bound=[0], max_bound=[40], phase=0)
-    u_bounds.add("tendons", min_bound=[0], max_bound=[40], phase=1)
+    u_bounds.add("tendons", min_bound=[0], max_bound=[5], phase=0)
+    u_bounds.add("tendons", min_bound=[0], max_bound=[5], phase=1)
 
     return OptimalControlProgram(
         bio_model,
         n_shooting=[50,50],
-        phase_time=(1,1),
+        phase_time=(1,0.8),
         objective_functions=objective_functions,
         constraints=constraints,
         x_bounds=x_bounds,
         u_bounds=u_bounds,
-        phase_transitions=phase_transitions
+        #phase_transitions=phase_transitions,
+        variable_mappings=dof_mapping,
+        n_threads=10
     )
 
 def main():
-    ocp = prepare_ocp(ExampleUtils.folder + "/models/tendon_2dof_finger_with_contact.bioMod")
+    ocp = prepare_single_phase_ocp(ExampleUtils.folder + "/models/tendon_2dof_finger_with_contact.bioMod")
     ocp.print(to_console=False, to_graph=False)
     ocp.add_plot_penalty(CostType.CONSTRAINTS)
     sol = ocp.solve(Solver.IPOPT())
--- tests/shard1/test_tendon_dynamics.py
+++ tests/shard1/test_tendon_dynamics.py
@@ -3,7 +3,7 @@ from types import SimpleNamespace
 
 import numpy as np
 import pytest
-from casadi import DM, Function, MX
+from casadi import DM, Function, MX, vertcat
 
 from bioptim import DynamicsFunctions, TendonBiorbdModel
 from tests.utils import TestUtils
@@ -26,6 +26,22 @@ def _evaluate_tau_from_tendons(nlp, q_value, qdot_value, tendon_pull_forces_valu
     tau_fun = Function("tau_from_tendons", [q, qdot, tendon_pull_forces], [tau])
     return tau_fun(q_value, qdot_value, tendon_pull_forces_value)
 
+def _integrate_RK4(times, start_states, controls, dyn_fun, time_between_nodes, n_shooting, n_steps, nb_q, nb_qdot):
+    integrated_states = DM.zeros((nb_q+nb_qdot, n_shooting+1))
+    integrated_states[:, 0] = start_states
+    h = time_between_nodes / n_steps
+    for i in range(n_shooting):
+        state_at_i = integrated_states[:, i]
+        control_at_i = controls[:, i]
+        for j in range(n_steps):
+            k1 = dyn_fun(state_at_i, control_at_i)
+            k2 = dyn_fun(state_at_i + h/2 * k1, control_at_i)
+            k3 = dyn_fun(state_at_i + h/2 * k2, control_at_i)
+            k4 = dyn_fun(state_at_i + h * k3, control_at_i)
+            state_at_i += h/6 * (k1 + 2 * k2 + 2 * k3 + k4)
+        integrated_states[:, i+1] = state_at_i
+    return integrated_states
+
 
 @pytest.mark.parametrize(
     "q_value,qdot_value,tendon_pull_forces_value,expected_tau",
@@ -46,3 +62,54 @@ def test_compute_tau_from_tendons_matches_expected_values(
     tau = _evaluate_tau_from_tendons(nlp, q_value, qdot_value, tendon_pull_forces_value)
 
     np.testing.assert_allclose(np.array(tau).squeeze(), expected_tau, rtol=1e-6, atol=1e-6)
+
+@pytest.mark.parametrize(
+    "q_start_value,qdot_start_value,tendon_pull_forces_value",
+    [
+        (DM([0.0, 0.0]), DM([0.0, 0.0]), DM([[50]*10])),
+    ]
+)
+def test_dynamics_explicitly(q_start_value, qdot_start_value, tendon_pull_forces_value):
+    model_path = TestUtils.bioptim_folder() + "/examples/models/tendon_test_finger.bioMod"
+    model = TendonBiorbdModel(model_path)
+    nlp = FakeNlp(model)
+
+    states_sym = MX.sym("Q_Qdot", model.nb_q + model.nb_qdot, 1)
+    controls_sym = MX.sym("Tendons", 1, 1)
+    tau_dyn = DynamicsFunctions.compute_tau_from_tendons(
+        nlp,
+        states_sym[:model.nb_q],
+        states_sym[model.nb_q:],
+        controls_sym
+    )
+    rhs = vertcat(
+        states_sym[model.nb_q :],
+        nlp.model.forward_dynamics()(states_sym[: model.nb_q], states_sym[model.nb_q :], tau_dyn, [], [])
+    )
+    dyn_fun = Function(
+        "dynamics",
+        [states_sym, controls_sym],
+        [rhs]
+    )
+
+    time_between_nodes = 0.05
+    n_shooting = 10
+    n_steps = 5
+    times = [time_between_nodes*i for i in range(n_shooting)]
+    start_states = vertcat(q_start_value, qdot_start_value)
+    controls = tendon_pull_forces_value
+
+    states_integrated = _integrate_RK4(
+        times,
+        start_states,
+        controls,
+        dyn_fun,
+        time_between_nodes,
+        n_shooting,
+        n_steps,
+        model.nb_q,
+        model.nb_qdot
+    )
+
+    print("Integrated states:\n", states_integrated)
+    np.testing.assert_almost_equal(np.array(states_integrated), np.array([[0]*(n_shooting+1)]*(model.nb_q+model.nb_qdot)))
